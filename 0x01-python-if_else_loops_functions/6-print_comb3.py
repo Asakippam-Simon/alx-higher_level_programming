06239b143bc80d1e6151fda5fcf8c5a3f73f6f7a
Asakippam-Simon <simonasakipaam@gmail.com>
2023-01-04 18:18:22 -0800

    Adding a file

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 for i in range(0, 10):
     for j in range(i+1, 10):
-        #if i >= j:
-         #   continue
-        if i == 8 and j == 9:
+        if i >= j:
+            continue
+        elif i == 8 and j == 9:
             print("{}{}".format(i, j))
         else:
             print("{}{}, ".format(i, j), end="")
