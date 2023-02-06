7f82ed2fceca9b819d8073bb167a389a9f14a205
Asakippam-Simon <simonasakipaam@gmail.com>
2023-02-06 10:20:45 -0800

    Adding lookup

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+=============================
+Module with the method lookpu
+=============================
+"""
+
+
+def lookup(obj):
+    """Function for returning
+    the attributes for an object"""
+
+    return dir(obj)
