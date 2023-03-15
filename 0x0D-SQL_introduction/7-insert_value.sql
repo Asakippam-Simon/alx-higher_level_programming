9726bd0c71a5d1653e17d0f9876c99a7f99d6ffc
Asakippam-Simon <simonasakipaam@gmail.com>
2023-03-14 18:00:10 -0700

    Adding file

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,2 +1,2 @@
 -- Insert a new row in the table first_table.
-INSERT INTO `first_table` (`id`, `name`) VALUES (89, "hbtn_0c_0");
+INSERT INTO `first_table` (`id`, `name`) VALUES (89, "Best School");
