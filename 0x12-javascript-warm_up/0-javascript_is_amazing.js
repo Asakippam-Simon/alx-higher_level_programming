7f12d86f865904a4d488826b7cf5c7cf4ff951ba
Asakippam-Simon <simonasakipaam@gmail.com>
2023-04-12 02:10:36 +0000

    Adding file

--- 0x12-javascript-warm_up/0-javascript_is_amazing.js
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -1,4 +1,5 @@
 #!/usr/bin/node
-// script that "Javascript is amazingprints" 
+// script that "Javascript is amazingprints"
+ 
 const myVar = 'Javascript is amazing';
-console.log(myVar);  
+console.log(myVar);
