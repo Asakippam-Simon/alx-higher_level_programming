4715501083a4cdb774ddab8a8e63062e218cff63
Asakippam-Simon <simonasakipaam@gmail.com>
2023-04-13 08:44:25 -0700

    Updating file

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,7 +1,9 @@
 #!/usr/bin/node
-/* script that prints 3 lines: (like 1-multi_languages.js)
-but by using an array of string and a loop */
-const myArray = ['C is fun', 'Python is cool', 'Javascript is amazing'];
-for (let i = 0; i < myArray.length; i++) {
-  console.log(myArray[i]);
+// prints 3 lines: (like 1-multi_languages.js) but by
+// using an array of string and a loop
+
+const langs = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+
+for (let i = 0; i < 3; i++) {
+  console.log(langs[i]);
 }
