2b12562f18afc13de370ab5db4d110676ff48a1e
Asakippam-Simon <simonasakipaam@gmail.com>
2023-04-28 16:09:04 -0700

    Adding file

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-"""  fetches https://intranet.hbtn.io/status  """
+"""  fetches https://alx-intranet.hbtn.io/status  """
 import urllib.request
 
 
 if __name__ == "__main__":
-    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
         r = response.read()
         print("Body response:")
         print("\t- type: {}".format(type(r)))
