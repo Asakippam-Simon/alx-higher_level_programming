4e44110f554ac576d970d0c9cb5f6149bab321a9
Asakippam-Simon <simonasakipaam@gmail.com>
2023-04-28 16:17:55 -0700

    Adding file

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
+"""displays the value of the X-Request-Id variable found in
+the header of the response.
 """
-Python script that fetches https://intranet.hbtn.io/status
-"""
-import requests
 
-if __name__ == '__main__':
 
-    res = requests.get("https://intranet.hbtn.io/status")
-    print("Body response:")
-    print("\t- type: {}".format(type(res.text)))
-    print("\t- content: {}".format(res.text))
+if __name__ == "__main__":
+    from requests import get
+
+    html = get('https://alx-intranet.hbtn.io/status')
+    print('Body response:')
+    print("\t- type: {}".format(html.text.__class__))
+    print("\t- content: {}".format(html.text))
